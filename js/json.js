bfa85502437825aa766e27e86223600c59831c81
Vedangi Patel <vedangipatel1005@gmail.com>
2025-11-23 12:36:59 -0500

    Begin with json.js file and called the populate function.

--- js/json.js
+++ js/json.js
@@ -6,7 +6,7 @@ const section = document.querySelector('section');
 async function populate(){
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
-    const requestURL = './js/i-scream.json';
+    const requestURL = '/js/i-scream.json';
     // STEP 5: Use the new URL to create a new request object
     //const request = new Request(requestURL);
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
@@ -27,7 +27,7 @@ populate();
 
 
 // STEP 3b: Call the populate() function
-
+populate();
 
 /* STEP 9b: Build out the populateHeader() function */
 function populateHeader(jsonObj) {
